f9de85432f6dc0e830e3d343f262a1165de9f07d
henry senyondo <henrykironde@gmail.com>
2022-04-19 23:58:13 -0400

    Add log date when updated (#28)
    * Update update_repo.sh
    
    * Reset local repo

--- update_repo.sh
+++ update_repo.sh
@@ -1,9 +1,10 @@
 changed=0
 git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
-    git pull
+    git reset --hard origin/main && git pull
     R -e "install.packages('devtools', repos='https://cran.rstudio.com/')"
     R -e "devtools::install_github('weecology/portalcasting', upgrade = TRUE)"
+    echo "Updated $(date)"
 else
     echo "Up-to-date $(date)"
 fi
