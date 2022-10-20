73c21a837b4726463e05c966d66a6365acbecba1
henrykironde <henrykironde@gmail.com>
2022-10-20 11:38:29 -0400

    Run setup dir regardless of changes.
    Output log is in ../cron_setup_dir.log

--- update_repo.sh
+++ update_repo.sh
@@ -4,17 +4,17 @@ if [ $changed = 1 ]; then
     git reset --hard origin/main && git pull
     R -e "install.packages('devtools', repos='https://cran.rstudio.com/')"
     R -e "devtools::install_github('weecology/portalcasting', upgrade = TRUE)"
-
-    # Update setup directory every friday 
-    friday_start=52335
-    friday_end=52359
-    Current_time=$(date +"%u%H%M")
-    if [ $friday_start -le $Current_time ] && [ $friday_end -gt $Current_time ]
-	    then
-            R -e "library(portalcasting); setup_production()"
-    	    echo "Directory is updated using setup_production $(date)"
-	    fi
     echo "Updated $(date)"
 else
     echo "Up-to-date $(date)"
 fi
+
+# Update setup directory every friday
+friday_start=52335
+friday_end=52359
+Current_time=$(date +"%u%H%M")
+if [ $friday_start -le $Current_time ] && [ $friday_end -gt $Current_time ]
+    then
+        R -e "library(portalcasting); setup_production()">../cron_setup_dir.log
+        echo "Directory is updated using setup_production $(date)">>../cron_setup_dir.log
+    fi
