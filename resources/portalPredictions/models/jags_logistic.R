33d84e975b497818620eb438e9d49ef3bc0c0d71
henrykironde <henrykironde@gmail.com>
2022-11-13 01:41:16 -0500

    Remove forecast and resource folder

--- resources/portalPredictions/models/jags_logistic.R
+++
@@ -1,2 +0,0 @@
-cast_dm_controls <- jags_logistic(dataset = "dm_controls", main = ".", settings = directory_settings(), quiet = FALSE, verbose = FALSE);
-save_cast_output(cast_dm_controls, main = ".", settings = directory_settings(), quiet = FALSE);
