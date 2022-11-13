2d4d202d522411edea25df57c2f395410deb2bad
henrykironde <henrykironde@gmail.com>
2022-11-13 01:42:06 -0500

    Use a helper file, util for shared objects.
    Co-authored-by: Juniper Simonis <dapperstats@gmail.com>
    Co-authored-by: Ethan White (he/him) <ethan@weecology.org>

--- app.R
+++ app.R
@@ -1,21 +1,4 @@
-
 library(shiny)
-library(portalcasting)
-
-
-# Render Rmd files
-rmarkdown::render("models.Rmd")
-
-# Data setup
-
-#setup_production()
-
-# Get lists of species as both abbreviations and scientific names
-
-species_names <- rodent_species(set = "base", type = "table")
-
-model_list <- c(prefab_models(), "Ensemble")
-model_list <- model_list[!(model_list %in% c("jags_logistic", "jags_logistic_covariates"))]
 
 # Run the application 
-runApp()
+shinyApp(ui = ui, server = server)
---
+++ util.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(portalcasting)
+
+
+# Render Rmd files
+rmarkdown::render("models.Rmd")
+
+# Data setup
+
+# Need to set this up to only run periodically external
+# to the shiny app
+# setup_production()
+
+# Get lists of species as both abbreviations and scientific names
+
+species_names <- rodent_species(set = "base", type = "table")
+
+model_list <- c(prefab_models(), "Ensemble")
+model_list <- model_list[model_list != "jags_logistic"]
