24cad6d3ad5bdad52029f51499b113a613f2c020
juniperlsimonis <dapperstats@gmail.com>
2023-04-26 10:16:26 -0700

    Update test_app_build.R

--- tests/test_app_build.R
+++ tests/test_app_build.R
@@ -1,6 +1,10 @@
-test_wrap_function <- function(appDir = ".") {
+# does app directory exist with necessary content to build a full website?
 
-  
+test_wrap_function <- function(main = ".") {
+
+  app_exists <- file.exists(main)
+
+  global <- global_list(main = main)
 
 }
 test_wrap_function()
