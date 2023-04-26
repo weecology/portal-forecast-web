feee97d5f375184ffe368dda2b8e2306bd229caa
juniperlsimonis <dapperstats@gmail.com>
2023-04-26 10:54:09 -0700

    Update test_app_build.R

--- tests/test_app_build.R
+++ tests/test_app_build.R
@@ -1,10 +1,12 @@
 # does app directory exist with necessary content to build a full website?
 
+library(portalcasting)
+
 test_wrap_function <- function(main = ".") {
 
   app_exists <- file.exists(main)
   update_dir(main = main)
-  global <- portalcasting::global_list(main = main)
+  global <- global_list(main = main)
 
 }
 test_wrap_function()
